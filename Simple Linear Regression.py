e88fa220363cd21151c21d0aba7558a1fbe8efe5
Aamir Syed <iapetus333@gmail.com>
2019-12-25 01:20:27 +0530

    Add files via upload

--- Simple Linear Regression.py
+++ Simple Linear Regression.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[102]:
-
-
 import numpy as np 
 import pandas as pd
 from sklearn.linear_model import LinearRegression
@@ -13,35 +7,15 @@ from numba import jit, njit
 import warnings
 warnings.simplefilter('ignore')
 
-
-# In[2]:
-
-
 df = pd.read_csv("dataset.csv")
 
-
-# In[4]:
-
-
 x,y = list(df['Subject 1']), list(df['Subject 2'])
-
-
-# In[18]:
-
 
 @jit(parallel = True, fastmath=True)
 def mean(arr):
     return sum(arr)/len(arr);
 
-
-# In[103]:
-
-
 mean_x, mean_y = mean(x), mean(y)
-
-
-# In[80]:
-
 
 @jit(parallel=True,fastmath=True)
 def get_bias_and_constant(arr, l, mean_x=mean_x, mean_y=mean_y):
@@ -56,15 +30,7 @@ def get_bias_and_constant(arr, l, mean_x=mean_x, mean_y=mean_y):
     
     return bias, constant
 
-
-# In[104]:
-
-
 m,c = get_bias_and_constant(arr=x, l=y, mean_x=mean_x, mean_y=mean_y)
-
-
-# In[128]:
-
 
 @jit(parallel=True, fastmath=True)
 def metadata_stats(x=x,y=y,mean_x = mean_x, mean_y = mean_y,pred_y=pred_y,m=m,c=c):
@@ -78,73 +44,25 @@ def metadata_stats(x=x,y=y,mean_x = mean_x, mean_y = mean_y,pred_y=pred_y,m=m,c=
     print("Squared Error : {}".format(sq_err[0]))
 
 
-# In[57]:
-
-
 df['Predicted'] = df['Subject 1'].apply(lambda x : (m*x +c))
-
-
-# In[61]:
-
 
 lr = LinearRegression()
 lr.fit(np.array(x).reshape(-1,1),np.array(y).reshape(-1,1))
 
-
-# In[64]:
-
-
 pred_y = lr.predict(np.array(x).reshape(-1,1))
-
-
-# In[72]:
-
-
 pr_y = [pred_y[i][0] for i in range(len(pred_y))]
 
-
-# In[82]:
-
-
 df['Library Prediction']=pr_y
-
-
-# In[98]:
-
 
 @jit(fastmath=True)
 def calculate_error(col1, col2):
     return col1-col2
 
-
-# In[99]:
-
-
 df['Absolute error']=df.apply(lambda x : calculate_error(x['Predicted'], x['Library Prediction']), axis=1)
-
-
-# In[100]:
-
 
 df.head(5)
 
-
-# In[130]:
-
-
 print("Metadata Stats (Predicted):\n")
 metadata_stats()
-
-
-# In[132]:
-
-
 print("Metadata Stats (Library) :\n")
 print("Intercept  : {}\nBias : {}\nSquared Error : {}".format(lr.intercept_[0],lr.coef_[0][0],lr.score(np.array(x).reshape(-1,1),np.array(y).reshape(-1,1))))
-
-
-# In[ ]:
-
-
-
-
